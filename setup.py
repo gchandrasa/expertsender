cd9aea1addd55be7c9d8adfb91137d9581af9317
Gilang Chandrasa <gilang.chandrasa@gmail.com>
2014-11-07 15:58:18 +0700

    Add minimum setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='expertsender',
+    version='0.1.0',
+    description='Python API library for the ExpertSender email platform',
+    author='Gilang Chandrasa',
+    author_email='gilang@launchpotato.com',
+    url='https://github.com/gchandrasa/expertsender',
+    packages=[
+        'expertsender',
+    ],
+    package_dir={'expertsender':
+                 'expertsender'},
+    install_requires=[
+        "requests",
+        "lxml",
+    ],
+)
